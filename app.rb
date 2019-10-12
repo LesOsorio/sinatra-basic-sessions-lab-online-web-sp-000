d82bc24995c4666220253b73a60a52dd753d0892
Leslie Osorio <lesosorio17@gmail.com>
2019-10-12 19:29:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,12 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/hey' do
-    @session = session
-    erb :index
-  end
-
   get '/' do
+    @session = session
     erb :index
   end
 
