746afa3b157c08e4764273e581d0121835b92b24
Leslie Osorio <lesosorio17@gmail.com>
2019-10-12 19:27:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,10 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do
+  get '/hey' do
     @session = session
     erb :index
   end
+
+  get '/' do 
+    erb :index
 
   post '/checkout' do
     @item = params["item"]
